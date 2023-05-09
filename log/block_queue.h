3b2b57670e881f8856f05656eb54ca99651c1a4e
Emptyskyv <Emptyskyv@gmail.com>
2023-05-09 21:00:58 +0800

    5.9

---
+++ log/block_queue.h
@@ -0,0 +1,8 @@
+//
+// Created by chenpengxu.emptysky on 5/9/23.
+//
+
+#ifndef LEARNING_TINYWEBSERVER_BLOCK_QUEUE_H
+#define LEARNING_TINYWEBSERVER_BLOCK_QUEUE_H
+
+#endif //LEARNING_TINYWEBSERVER_BLOCK_QUEUE_H
---
+++ log/log.h
@@ -0,0 +1,8 @@
+//
+// Created by chenpengxu.emptysky on 5/9/23.
+//
+
+#ifndef LEARNING_TINYWEBSERVER_LOG_H
+#define LEARNING_TINYWEBSERVER_LOG_H
+
+#endif //LEARNING_TINYWEBSERVER_LOG_H
